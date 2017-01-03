923957a2b449444e93fc610c0dacee37293a1f25
samuelsmith <samuel.smith.hk@gmail.com>
2017-01-03 17:13:45 -0500

    Half way through implementation and testing, also includes gradle and idea gubbings

---
+++ src/main/java/game/Card.java
@@ -0,0 +1,15 @@
+package game;
+
+/**
+ * Created by samuelsmith on 1/2/17.
+ */
+public class Card {
+
+    public static Card fromCharacter(char c) {
+        return new Card();
+    }
+
+    private Card() {
+
+    }
+}
---
+++ src/main/java/game/Deck.java
@@ -0,0 +1,12 @@
+package game;
+
+/**
+ * Created by samuelsmith on 1/2/17.
+ */
+public class Deck {
+
+    public Card nextCard() {
+        return null;
+    }
+
+}
